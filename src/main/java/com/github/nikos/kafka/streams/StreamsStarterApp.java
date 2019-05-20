58b1231c13e68d0429b5e863047ffce73033e1eb
nikapa21 <nikapa21@gmail.com>
2019-05-20 22:58:54 +0300

    -First commit for Kafka Streams Starter App working on top of Kafka Broker Cluster, receiving input topics of bus line ids, doing some sampling operation and, sending output topics to consumers through the cluster

---
+++ src/main/java/com/github/nikos/kafka/streams/StreamsStarterApp.java
@@ -0,0 +1,47 @@
+package com.github.nikos.kafka.streams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class StreamsStarterApp {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-starter-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+
+        KStream<String, String> wordCountInput = builder.stream("topic-036-input");
+
+        KTable<String, Long> wordCounts = wordCountInput
+                .mapValues(value -> value
+                .toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((ignoredKey, word) -> word)
+                .groupByKey()
+                .count("Counts");
+
+        wordCounts.to(Serdes.String(),Serdes.Long(),"topic-036-output");
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
+
+        // printing the topology
+        System.out.println(streams.toString());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+}
